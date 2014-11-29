28494bbd72f1ea3b8881531c737c3edbb77216f0
Jonathan Pulfer <JonPulfer@users.noreply.github.com>
2014-11-29 22:27:43 +0000

    Added single article view and made the title a link to the article.

--- public/js/SailingNews/router.js
+++ public/js/SailingNews/router.js
@@ -1,10 +1,29 @@
 SailingNews.Router.map(function() {
 	this.resource('articles', { path: '/' });
+	this.route('article', { path: '/article/:article_id'});
 });
 
 SailingNews.ArticlesRoute = Ember.Route.extend({
  	model: function() {
-    // the model is an Array of all of the articles
-    return this.store.find('article');
-  }
+    	// the model is an Array of all of the articles
+    	return this.store.find('article');
+  	},
+	renderTemplate: function() {
+   		this.render({
+			into: 'application',
+			outlet: 'content',
+		});
+  	},
 });
+
+SailingNews.ArticleRoute = Ember.Route.extend({
+	model: function(params) {
+		return this.store.find('article', params.article_id);
+	},
+	renderTemplate: function() {
+   		this.render({
+			into: 'application',
+			outlet: 'content',
+		});
+  	},
+})
